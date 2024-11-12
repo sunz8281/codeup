ad7e115d2fd3984221ca3338b091a476b41fb54e
sunz8281 <sunz8281@gmail.com>
2024-11-12 15:08:03 +0900

    fix 3701

--- C/3701.c
+++ C/3701.c
@@ -7,7 +7,7 @@ int main(void) {
 
     for(int i=1; i<=n; i++, puts("")) {
         for(int j=1; j<=i; j++) {
-            if(i==1||j==1) arr[i][j]=1;
+            if(j==1||i==j) arr[i][j]=1;
             else arr[i][j] = arr[i-1][j] + arr[i-1][j-1];
             printf("%lld ", arr[i][j]);
         }
